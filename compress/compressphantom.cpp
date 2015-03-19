1ff55e1e5b36984f0adf94020cf4aedd913ec4cb
ben <ben.spoor@gmail.com>
2015-03-19 23:41:58 +0100

    Fixed lint and compile issues in compressphantom.cpp

--- compress/compressphantom.cpp
+++ compress/compressphantom.cpp
@@ -1,75 +1,67 @@
 #include <iostream>
 #include <fstream>
-#include "phantom.h"
+#include "./phantom.h"
 
-using namespace std;
+using std::cout;
+using std::endl;
 
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cout << "Error! An input file is needed " << endl;
+        return -1;
+    }
+    char inphanfile[60], outphanfile[60], displfile[60];
+    int dispSize;
 
-int main(int argc, char* argv[])
-{
+    FILE *fpinput;
 
-	if(argc<2) {
-		cout<<"Error! An input file is needed "<<endl;
-		return -1;
-	}
-	char inphanfile[60], outphanfile[60], displfile[60], line[60];
-	int dispSize;
+    if ((fpinput=fopen(argv[1], "r")) == NULL) {
+        cout << "Can't find input file!" << endl;
+        return -1;
+    }
 
-	FILE *fpinput;
-	
-	if ((fpinput=fopen(argv[1], "r"))==NULL)
-		{
-			cout<<"Can't find input file!"<<endl;
-			return -1;
-		}
+    while (fgetc(fpinput) != ':') {}
+    fscanf(fpinput, "%s", inphanfile);
 
-	while (fgetc(fpinput)!=':');
-	fscanf(fpinput, "%s", inphanfile);
-	
+    while (fgetc(fpinput) != ':') {}
+    fscanf(fpinput, "%s", outphanfile);
 
-	while (fgetc(fpinput)!=':');
-	fscanf(fpinput, "%s", outphanfile);
+    while (fgetc(fpinput) != ':') {}
+    fscanf(fpinput, "%s", displfile);
 
-	while (fgetc(fpinput)!=':');
-	fscanf(fpinput, "%s", displfile);
+    while (fgetc(fpinput) != ':') {}
+    fscanf(fpinput, "%d", &dispSize);
 
-	while (fgetc(fpinput)!=':');
-	fscanf(fpinput, "%d", &dispSize);
+    phantom target;
+    cout << "Pre Phantom File name is: " << inphanfile << endl;
+    cout << "Output Phantom file name will be: " << outphanfile << endl;
+    cout << "The displacement matrix has a size of: " << dispSize << endl;
 
-	
-	phantom target;
-	cout<<"Pre Phantom File name is: " <<inphanfile <<endl;
-	cout <<"Output Phantom file name will be: " <<outphanfile <<endl;
-	cout <<" The displacement matrix has a size of:"  << dispSize <<endl;
-	
-	target.loadPhantom(inphanfile);
+    target.loadPhantom(inphanfile);
 
+    // this bit of the code allocates memory to contain a dispSize by dispSize
+    //       array of x and y displacements
+    double *u, *v;
+    u = new double[dispSize*dispSize];
+    v = new double[dispSize*dispSize];
 
- 	  //this bit of the code allocates memory to contain a dispSize by dispSize array of x and y displacements
-		double *u, *v;
-        u= new double[dispSize*dispSize];
-		v= new double[dispSize*dispSize];
-	
-		ifstream fpdisp;
-		fpdisp.open(displfile, ios::binary);
-		if(!fpdisp.is_open())
-		{
-			cout<<"Error! Can't find file "<<displfile<<endl;
-			return -1;
-		}
-		
-        //here the displacements are read in from the .dis file
-        fpdisp.read((char*)u,dispSize*dispSize*sizeof(double) );
-		fpdisp.read((char*)v,dispSize*dispSize*sizeof(double) );
-		fpdisp.close();
+    std::ifstream fpdisp;
+    fpdisp.open(displfile, std::ios::binary);
+    if ( !fpdisp.is_open() ) {
+        cout << "Error! Can't find file " << displfile << endl;
+        return -1;
+    }
 
+    // here the displacements are read in from the .dis file
+    fpdisp.read(reinterpret_cast<char*>(u), dispSize*dispSize*sizeof(double));
+    fpdisp.read(reinterpret_cast<char*>(v), dispSize*dispSize*sizeof(double));
+    fpdisp.close();
 
-		target.displaceAnsys(u,v,dispSize);
+    target.displaceAnsys(u, v, dispSize);
 
-        target.savePhantom(outphanfile);
+    target.savePhantom(outphanfile);
 
-		return 0;
-	
+    return 0;
 }
 
 
