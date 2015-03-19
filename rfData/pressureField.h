a075ae23503e8c635067d2bbfb42279827e6d81d
ben <ben.spoor@gmail.com>
2015-03-19 20:21:51 +0100

    Fixed cpplint issues

--- rfData/pressureField.h
+++ rfData/pressureField.h
@@ -2,7 +2,7 @@
 #define __PRESSUREFIELD_H__
 
 #define FULL_APERTURE -1
-#include <stdio.h> 
+#include <stdio.h>
 #include <assert.h>
 #include "phantom.h"
 
@@ -14,8 +14,8 @@ class phantom;
 //structure used to describe a single rectangular element
 struct singleGeom
 {
-	double width;
-	double length;
+    double width;
+    double length;
 
 };
 
@@ -23,43 +23,43 @@ struct singleGeom
 
 class array
 {
-	friend class fieldBuffer;
+    friend class fieldBuffer;
 
 
-	public:
-		array(singleGeom, double, int, double);
-		//(geom, spacing between elements, element number, assumed sound speed)
+    public:
+        array(singleGeom, double, int, double);
+        //(geom, spacing between elements, element number, assumed sound speed)
 
 
-		~array(); //destructor
+        ~array(); //destructor
 
 
-		void setTransFocus(double, double, double);
-		//(focal distance, angle of steering, F number, frequency) 
-		//set F<0 to active all the elements
+        void setTransFocus(double, double, double);
+        //(focal distance, angle of steering, F number, frequency)
+        //set F<0 to active all the elements
 
-		void setRecFocus(double, double, double);
-		//(focal distance, angle of steering, F number, frequency)
-		//set F<0 to active all the elements
+        void setRecFocus(double, double, double);
+        //(focal distance, angle of steering, F number, frequency)
+        //set F<0 to active all the elements
 
-		double Spacing() { return spacing;};
-		//get the spacing
-		double trsFnum() { return trnsFnum;};
-		double recFnum() { return recvFnum;};
-		void setTrsFnum(double);
-		void setRecFnum(double);
-	private:
-		void setFocus(cplx*, double, double, double);
-		//common routine to set lateral focus
-		//(phase factor[], focal distance, F number, frequency);
+        double Spacing() { return spacing;};
+        //get the spacing
+        double trsFnum() { return trnsFnum;};
+        double recFnum() { return recvFnum;};
+        void setTrsFnum(double);
+        void setRecFnum(double);
+    private:
+        void setFocus(cplx*, double, double, double);
+        //common routine to set lateral focus
+        //(phase factor[], focal distance, F number, frequency);
 
-		singleGeom geom; //hold the elevational geometry info
-		double spacing; //spacing between elements
-		int eleCnt; //number of elements
-		double trnsFnum, recvFnum;
-		cplx *transPhase; //transmit lateral phase factors 
-		cplx *recPhase; //receive lateral phase factors
-		double assumedSoundSpeed;
+        singleGeom geom; //hold the elevational geometry info
+        double spacing; //spacing between elements
+        int eleCnt; //number of elements
+        double trnsFnum, recvFnum;
+        cplx *transPhase; //transmit lateral phase factors
+        cplx *recPhase; //receive lateral phase factors
+        double assumedSoundSpeed;
 
 };
 
@@ -68,54 +68,53 @@ class array
 
 class fieldBuffer   //incident pressure field associated with a phantom
 {
-	public:
-		fieldBuffer(double, double, const vector&, phantom*, double, array*, double);
-		//constructor (transmit focus, beam width, grid step );
+    public:
+        fieldBuffer(double, double, const vector&, phantom*, double, array*, double);
+        //constructor (transmit focus, beam width, grid step );
 
-		~fieldBuffer(); //destructor
+        ~fieldBuffer(); //destructor
 
-		void calculateBufferField(double);
-		//calculate the buffer at frequency (freq)
-		
-		cplx bufferField(const vector&);		
-		//get the pressure field at (location)
-		void beamProfile();
+        void calculateBufferField(double);
+        //calculate the buffer at frequency (freq)
 
-		vector giveCenter() {return center; };
-		double giveImageDepth() {return size.z;};
+        cplx bufferField(const vector&);
+        //get the pressure field at (location)
+        void beamProfile();
 
-		double giveSoundSpeed() {return transducer->assumedSoundSpeed;};
-		//get the pressure field contribution due to a single element
-		cplx getSingleElementField(const vector&, const singleGeom&, const cplx&);
+        vector giveCenter() {return center; };
+        double giveImageDepth() {return size.z;};
 
-		
-		//given a scatterer, return the coordinates relative to the transducer
-		vector phantomCoordinateToPressureCoordinate(const scatterer&, int);
-	private:
-		double transFocus; //Transmit focus
-		vector size; //the field size to be calculated
-		vector step; //the grid step
-		vector center; //center of the field
-		cplx K; //cplx wavenumber
-		phantom* target;
-		int xLen; //x dimension
-		int xLenExtra; //
-		int yLen; //y dimension
-		int zLen; //z dimension
-		int denseFactor; //densefactor is the number of calculated points along each element
-
-		int arrayLineSize; //buffer line size
-		int arrayPlaneSize; //buffer plane size
+        double giveSoundSpeed() {return transducer->assumedSoundSpeed;};
+        //get the pressure field contribution due to a single element
+        cplx getSingleElementField(const vector&, const singleGeom&, const cplx&);
 
 
-		cplx *singleRowTransField; //single row of the transmit field, constant depth
-		cplx *singleRowRecField; //single row of the receive field, constant depth
-	
-		cplx *arrayField; //resulting buffer field */
-		array* transducer;
-		double assumedSoundSpeed;
-		fresnelInt *fres;
-		double phantomGap;
+        //given a scatterer, return the coordinates relative to the transducer
+        vector phantomCoordinateToPressureCoordinate(const scatterer&, int);
+    private:
+        double transFocus; //Transmit focus
+        vector size; //the field size to be calculated
+        vector step; //the grid step
+        vector center; //center of the field
+        cplx K; //cplx wavenumber
+        phantom* target;
+        int xLen; //x dimension
+        int xLenExtra; //
+        int yLen; //y dimension
+        int zLen; //z dimension
+        int denseFactor; //densefactor is the number of calculated points along each element
+
+        int arrayLineSize; //buffer line size
+        int arrayPlaneSize; //buffer plane size
+
+        cplx *singleRowTransField; //single row of the transmit field, constant depth
+        cplx *singleRowRecField; //single row of the receive field, constant depth
+
+        cplx *arrayField; //resulting buffer field */
+        double assumedSoundSpeed;
+        array* transducer;
+        fresnelInt *fres;
+        double phantomGap;
 };
 
 
